3a9d0dbb19435aa1347c2a58064aa2caaef81f33
AkopovaAnna <anna.akopova.1989@gmail.com>
2021-01-22 01:04:43 +0400

    note management

---
+++ src/main/java/com/project/noteManagement/NoteManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.noteManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/noteManagement/NoteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.noteManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
